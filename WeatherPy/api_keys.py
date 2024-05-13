d1fcf9ff27988799bf00dfabf73a4ce22d01da0c
Brennan Bradley <Brennanb572@gmail.com>
2024-05-12 21:13:08 -0700

    updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "90bb51568e3cd9f33e1992a00cd54f96"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0321d351590b48d79bbe0e6d12cd0746"
